00c1d13f325c3af48db37c1917bd174a9a1ab3f8
Pankotaiimre2001 <pankotaiimre@gmail.com>
2023-11-01 16:32:10 +0100

    megdurvabb

--- rector/src/Contact.js
+++ rector/src/Contact.js
@@ -1,7 +1,36 @@
 import React from 'react'
 function Contact()
 {
-    return (<div>fnjkfnknwkjfwejfnewjk</div>);
+    return (<div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+        <div style={{width: 1424, height: 75, left: 0, top: 0, position: 'absolute', background: '#E1AD01'}} />
+        <img style={{width: 76, height: 76, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/76x76" />
+        <div style={{width: 211, height: 41, left: 964, top: 17, position: 'absolute', textAlign: 'center', color: '#8F0000', fontSize: 32, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>CONTACT US</div>
+        <div style={{width: 114, height: 29, left: 229, top: 19, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>HOME</div>
+        <div style={{width: 143, height: 29, left: 479, top: 20, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>ANIMALS</div>
+        <div style={{width: 168, height: 42, left: 720, top: 15, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>ABOUT</div>
+        <div style={{width: 114, height: 29, left: 1252, top: 22, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>FAQ</div>
+        <div style={{width: 1424, height: 105, left: 0, top: 1463, position: 'absolute', background: '#E1AD01'}}></div>
+        <img style={{width: 103, height: 103, left: 0, top: 1463, position: 'absolute'}} src="https://via.placeholder.com/103x103" />
+        <div style={{width: 320, height: 72, left: 159, top: 1494, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>Zoo-tiful Deals LLC.</div>
+        <div style={{width: 272, height: 68, left: 740, top: 1500, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>info@zootifuldeals.com</div>
+        <div style={{width: 257, height: 58, left: 492, top: 1498, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>+36205446565</div>
+        <div style={{width: 375, height: 56, left: 1045, top: 1499, position: 'absolute', textAlign: 'center', color: '#FFFDFD', fontSize: 24, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>4034 Debrecen, Vágóhíd utca 3.</div>
+        <img style={{width: 1424, height: 556, left: 0, top: 75, position: 'absolute'}} src="https://via.placeholder.com/1424x556" />
+        <div style={{width: 890, height: 348, left: 267, top: 144, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 128, fontFamily: 'Righteous', fontWeight: '400', wordWrap: 'break-word'}}>Contact us</div>
+        <div style={{width: 249, height: 116, left: 254, top: 713, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 36, fontFamily: 'Righteous', fontWeight: '400', wordWrap: 'break-word'}}>Customer Service Team</div>
+        <div style={{width: 249, height: 116, left: 836, top: 713, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 36, fontFamily: 'Righteous', fontWeight: '400', wordWrap: 'break-word'}}>Farm</div>
+        <div style={{width: 385, height: 100, left: 188, top: 829, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>Contact Customer Support</div>
+        <div style={{width: 385, height: 100, left: 772, top: 806, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>Visit us at</div>
+        <div style={{width: 302, height: 83, left: 217, top: 887, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '400', wordWrap: 'break-word'}}>Monday-Friday</div>
+        <div style={{width: 302, height: 83, left: 820, top: 962, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '400', wordWrap: 'break-word'}}>Monday-Friday</div>
+        <div style={{width: 302, height: 83, left: 217, top: 985, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '400', wordWrap: 'break-word'}}>Saturday-Sunday</div>
+        <div style={{width: 246, height: 52, left: 258, top: 951, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '400', wordWrap: 'break-word'}}>8:00AM-4:00PM CET</div>
+        <div style={{width: 246, height: 52, left: 853, top: 1019, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '400', wordWrap: 'break-word'}}>8:00AM-4:00PM CET</div>
+        <div style={{width: 246, height: 52, left: 258, top: 1042, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '400', wordWrap: 'break-word'}}>10:00AM-2:00PM CET</div>
+        <div style={{width: 246, height: 59, left: 241, top: 1100, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>+36205446565</div>
+        <div style={{width: 276, height: 70, left: 232, top: 1138, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>info@zootifuldeals.com</div>
+        <div style={{width: 293, height: 110, left: 829, top: 894, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Raleway', fontWeight: '400', wordWrap: 'break-word'}}>4034 Debrecen, Vágóhíd utca 3.,Hajdú-Bihar vármegye, Hungary</div>
+    </div>);
 }
 
 export default Contact;
