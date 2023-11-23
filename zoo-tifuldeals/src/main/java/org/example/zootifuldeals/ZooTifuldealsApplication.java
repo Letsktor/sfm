c983630b58e897172a5a5af25209fe9fa08391f0
Eszter <hallok.eszter@gmail.com>
2023-11-23 20:05:26 +0100

    megveszek

--- zoo-tifuldeals/src/main/java/org/example/zootifuldeals/ZooTifuldealsApplication.java
+++ zoo-tifuldeals/src/main/java/org/example/zootifuldeals/ZooTifuldealsApplication.java
@@ -15,6 +15,7 @@ public class ZooTifuldealsApplication {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+
     }
     @GetMapping("/zold")
     public void cc(){
