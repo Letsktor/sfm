1540f7d2d9e2575fc997ef15491a6fa4b7510578
Tomi <szitomi@mailbox.unideb.hu>
2023-11-23 13:30:47 +0100

    Animals

---
+++ demo/src/main/java/com/example/demo/Animal.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "animals")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Animal {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String species;
+    private Float  rating;
+    private String name;
+    private Float price;
+    private String description;
+    private String picurl;
+}
