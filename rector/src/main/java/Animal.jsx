1932a0ab44caba8521c23fd4945d67bec744783f
Letsktor <vajasmate2013@gmail.com>
2023-11-24 18:53:57 +0100

    Working without email sending

--- rector/src/main/java/Animal.jsx
+++ rector/src/main/java/Animal.jsx
@@ -3,7 +3,9 @@ import logo from "../resources/Pictures/szovjetmaci_2.png";
 import React, {useEffect, useState} from "react";
 import {useNavigate,useLocation} from "react-router-dom";
 import axios from "axios";
-
+import "../resources/Css/Animal.css"
+import MyDatePicker from "./MyDatePicker"
+import MyForm from "./MyForm";
 function Animal(){
     const navigate = useNavigate();
     const location = useLocation();
@@ -24,6 +26,8 @@ function Animal(){
     const homelClick=()=>{
         navigate('/');
     };
+
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const [animals, setAnimals] = useState([])
     useEffect( () => {
         loadAnimals()
@@ -50,14 +54,17 @@ function Animal(){
             </div>
             <div className={"container"}>
                         {
-                            animals.filter(animal=>animal.id===buttonId).map((animal, index)=>(
+
+                            animals.filter(animal=>animal.id===buttonId).map((animal, index)=>{
+                                return(
+
                                 <div>
                                     <div style={{height:250}}>
                                         <img style={{width: '100%', height: '100%',borderRadius: 10,objectFit:"cover"}} src={animal.picurl} alt={"gg"}/>
                                         <div style={{position:"absolute", top:'10%',left:"30%",color:"white",textShadow:"-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black",fontWeight: "bold",fontSize:128}}>{animal.name}</div>
                                     </div>
                                     <div style={{display:"flex",height:"10%",paddingTop:"1%"}}>
-                                        <div style={{backgroundColor:"lightgray",width:"50%",height:"100%", borderRadius: 10}}>
+                                        <div style={{backgroundColor:"lightgray",width:"50%",height:500, borderRadius: 10,justifyContent:"Center",display:"flex"}}>
                                             <img style={{width:"100%",height:"100%",borderRadius: 10,objectFit:"contain"}} src={animal.picurl} alt={"gg"}/>
                                         </div>
                                         <div style={{width:"50%",height:"100%", borderRadius: 10}}>
@@ -67,9 +74,23 @@ function Animal(){
                                             <div style={{paddingLeft:"10%",paddingTop:'1%',color:"white",textShadow:"-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black",fontSize:18}}>Price: {animal.price}$/h</div>
                                         </div>
                                     </div>
-                                    <div>FASZ</div>
+                                    <div style={{backgroundColor:"darkred",color:"white",display:"grid",justifyContent:"center"}}>
+                                        <div style={{fontSize:36,fontWeight:"Bold",paddingBottom:40}}>
+                                            Book a party with {animal.name}:
+                                        </div>
+                                        <div style={{display:"flex",flexDirection:"column",paddingBottom:60,justifyContent:"center"}}>
+                                            <div style={{marginBottom:"0.5em",fontWeight:"bold"}}>Pick your Date:</div>
+                                            <MyDatePicker selectedDate={selectedDate} setSelectedDate={setSelectedDate} animalID={animal.id}/>
+                                            <div style={{display:"flex",flexDirection:"column",paddingTop:20}}>
+                                                <MyForm selectedDate={selectedDate} animalId={animal.id}/>
+                                            </div>
 
-                                </div>))
+                                        </div>
+
+
+                                    </div>
+
+                                </div>);})
                         }
             </div>
             <div className={"bottom-bar"}>
